3bcdb0f5585bd7cb7789bacf8de1fa1490b72378
Cavit Bagci <cavit.bagci04@gmail.com>
2024-02-15 20:51:27 +0300

    Project name changed

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PhoneBook
+namespace Contacts
 {
     internal class Program
     {
@@ -12,8 +12,6 @@ namespace PhoneBook
         {
             SelectPage selectPage = new SelectPage();
             selectPage.Rehber();
-
-           
         }
     }
 }
